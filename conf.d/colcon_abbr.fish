d2f772ce9bbd6ad4539342d12e681b9fb93bd734
杨旭 <yangx21@mails.tsinghua.edu.cn>
2022-11-09 16:43:29 +0800

    rm arg-complete

--- conf.d/colcon_abbr.fish
+++ conf.d/colcon_abbr.fish
@@ -6,9 +6,3 @@ set -q COLCON_ABBR_TEST_OPTIONS || set -U COLCON_ABBR_TEST_OPTIONS "--event-hand
 # Register abbreviations
 colcon_abbr_register
 
-# Use argcomplete if found
-if command -v register-python-argcomplete >/dev/null 2>&1
-    register-python-argcomplete --shell fish colcon | source
-else
-    echo "argcomplete was not found"
-end
