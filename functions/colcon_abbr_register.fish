63c482d360439823d02e80d61b7ddfc24d58c69d
yx <yangx21@mails.tsinghua.edu.cn>
2022-12-04 11:34:31 +0800

    add git alias

--- functions/colcon_abbr_register.fish
+++ functions/colcon_abbr_register.fish
@@ -1,24 +1,21 @@
 function colcon_abbr_register
     register_colcon_build
-    register_colcon_test
 end
 
 function register_colcon_build
-    $COLCON_ABBR_USE_SYMLINK && set symlink " --symlink-install" || set symlink ""
-    set -q COLCON_ABBR_ADDITIONAL_OPTIONS && set additional_options " $COLCON_ABBR_ADDITIONAL_OPTIONS" || set additional_options ""
-    set -q COLCON_ABBR_CMAKE_ARGS && set cmake_args " $COLCON_ABBR_CMAKE_ARGS" || set cmake_args ""
-
-    set -l cb "colcon build"
+    # $COLCON_ABBR_USE_SYMLINK && set symlink " --symlink-install" || set symlink ""
+    # set -q COLCON_ABBR_ADDITIONAL_OPTIONS && set additional_options " $COLCON_ABBR_ADDITIONAL_OPTIONS" || set additional_options ""
+    # set -q COLCON_ABBR_CMAKE_ARGS && set cmake_args " $COLCON_ABBR_CMAKE_ARGS" || set cmake_args ""
     
     # ros2 pkg create
     abbr -a rpc_c "ros2 pkg create --build-type ament_cmake" 
     abbr -a rpc_p "ros2 pkg create --build-type ament_python"
     
     # colcon build
-    abbr -a cb "$cb"
+    abbr -a cb "colcon build"
 
     # colcon build --packages-select
-    abbr -a cbps "$cb --packages-select"
+    abbr -a cbps "colcon build --packages-select"
 
     # ros2 service call
     abbr -a rsc "ros2 service call"
@@ -39,23 +36,17 @@ function register_colcon_build
     
     # git operation
     abbr -a ga "git add"
-    abbr -a gc "git commit -m"
+    abbr -a gaa "git add ."
+    abbr -a gc "git commit -m ''"
     abbr -a gp "git push"
     abbr -a gs "git status"
+    abbr -a gpl "git pull"
+    abbr -a grth "git reset --hard"
     
     # sudo apt 
     abbr -a sap "sudo apt update"
     abbr -a sag "sudo apt upgrade"
     abbr -a sai "sudo apt install"
+    abbr -a saa "sudo aot autoremove"
     
-end
-
-function register_colcon_test
-    set -q COLCON_ABBR_TEST_OPTIONS && set test_options " $COLCON_ABBR_TEST_OPTIONS" || set test_options ""
-
-    set -l ct "colcon test$test_options"
-
-    abbr -a ct "$ct"
-    abbr -a ctp "$ct --packages-up-to"
-    abbr -a ctps "$ct --packages-select"
 end
